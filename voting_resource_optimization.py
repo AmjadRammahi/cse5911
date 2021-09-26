263428701322c366f805ab9662a8023dd87bbd06
Ghostjs123 <1011collin@gmail.com>
2021-09-26 02:54:35 -0400

    Renamed constants and labeled unused globals

--- voting_resource_optimization.py
+++ voting_resource_optimization.py
@@ -3,11 +3,8 @@ import math
 import logging
 import argparse
 import numpy as np
-import simpy as sp
-import multiprocessing as mp
 import pandas as pd
 import scipy.stats as st
-from random import expovariate
 
 from src.util import set_logging_level
 from src.voter_sim import voter_sim
@@ -25,122 +22,142 @@ logger = logging.getLogger()
 
 # Placeholder for input parameters
 # Options: Apportionment, Allocation
-prob_type = 'apportionment'
+prob_type = 'apportionment'  # NOTE: unused
 # If apportionment, this is the maximum total that can be purchased
 # If allocation, this is the total number of machines available to be allocated
-max_machines = 60
+MAX_MACHINES = 60
 
 # General variables
 # first and last voting locations. This allows results to be calculated for a subset of location
-first_loc = 1
-last_loc = 5
-num_locations = last_loc - first_loc + 1
-# time increment in minutes
-time_delta = 0.5
-# Probability of rejecting the null hypothesis
-alphaValue = 0.05
-# Indifference zone parameter
-deltaIZ = 0.5
-num_replications = 100
+FIRST_LOC = 1
+LAST_LOC = 5
+NUM_LOCATIONS = LAST_LOC - FIRST_LOC + 1
+
+time_delta = 0.5  # NOTE: unused - time increment in minutes
+ALPHA_VALUE = 0.05  # Probability of rejecting the null hypothesis
+DELTA_IZ = 0.5  # Indifference zone parameter
+NUM_REPLICATIONS = 100
 
 # deluxe version variable placeholders
 # 5:30, 6:30, 7:30
-early_start = 5.5
-poll_start = 6.5
-poll_end = 19.5
-Poll_Hours = (poll_end - poll_start) * 24
-EarlyVoterHours = poll_start - early_start
+EARLY_START = 5.5  # NOTE: effectively unused
+POLL_START = 6.5
+POLL_END = 19.5
+Poll_Hours = (POLL_END - POLL_START) * 24  # NOTE: unused
+EarlyVoterHours = POLL_START - EARLY_START
 
 # waiting time of voter who waits the longest
-service_req = 30
+SERVICE_REQ = 30
 
 # Batch size
-BatchSize = 20
+BATCH_SIZE = 20
 
 # Specifies whether there is a minimum allocation requirement, and if so what it is
 # Check that this is being used correctly
-min_alloc_flg = 1
-min_alloc = 4
+MIN_ALLOC_FLG = 1
+MIN_ALLOC = 4
 # min_alloc = 175
 
 # Defining objective
-# Current state: only "Max", future state to include "Quantile" and "Average"
-Objective = 'Max'
+# Current state: only 'Max', future state to include 'Quantile' and 'Average'
+Objective = 'Max'  # NOTE: unused
 # Not used in MVP
 # ObjectiveQuantileValue = 0.95
 # Waiting time <= how many minutes
-mu0Value = 500
+mu0Value = 500  # NOTE unused
 
 # Arrival time periods
 # not currently used
 # TotalNoOfPeriods = 4
 
 # Voting times
-MinVotingMin = 6
-MinVotingMode = 8
-MinVotingMax = 12
-MinBallot = 0
-MaxVotingMin = 6
-MaxVotingMode = 10
-MaxVotingMax = 20
-MaxBallot = 10
+MIN_VOTING_MIN = 6
+MIN_VOTING_MODE = 8
+MIN_VOTING_MAX = 12
+MIN_BALLOT = 0
+MAX_VOTING_MIN = 6
+MAX_VOTING_MODE = 10
+MAX_VOTING_MAX = 20
+MAX_BALLOT = 10
 # Only single resource has been built
-NumberOfResources = 1
+NumberOfResources = 1  # NOTE: unused
 
 # Not yet used
 # check_in_times = [0.9, 1.1, 2.9]
 
 # Calculate number of batches
-num_batches = num_replications / BatchSize
+NUM_BATCHES = NUM_REPLICATIONS / BATCH_SIZE
 
 # Add voter average arrivals in minutes
-poll_open = poll_end - poll_start
+POLL_OPEN = POLL_END - POLL_START
 
 # Create results arrays
-avgResources = np.zeros(num_locations)
-avgWaitingTime = np.zeros(num_locations)
-MaxWaitingTime = np.zeros(num_locations)
-QuantWaitingTime = np.zeros(num_locations)
-WaitProbabilities = np.zeros(num_locations)
-MeanClosingTimes = np.zeros(num_locations)
+avgResources = np.zeros(NUM_LOCATIONS)  # NOTE: unused
+avgWaitingTime = np.zeros(NUM_LOCATIONS)  # NOTE: unused
+MaxWaitingTime = np.zeros(NUM_LOCATIONS)  # NOTE: unused
+QuantWaitingTime = np.zeros(NUM_LOCATIONS)  # NOTE: unused
+WaitProbabilities = np.zeros(NUM_LOCATIONS)  # NOTE: unused
+MeanClosingTimes = np.zeros(NUM_LOCATIONS)  # NOTE: unused
 
 # machine_df contains estimated voters, max voters, and ballot length
 
+# TODO: this num_tests is heavily used and it looks like Tian had a TODO here for it - Collin
 # placeholder for algorithm to determine next configuration to test. This should be moved to location loop.
 num_test = 2
+
 # Iterate over locations
-loc_sol = np.zeros(num_locations)  # number machines
-loc_waits = np.zeros(num_locations)  # voter wait times
-loc_ct = np.zeros(num_locations)
+loc_sol = np.zeros(NUM_LOCATIONS)  # NOTE: unused - number machines
+loc_waits = np.zeros(NUM_LOCATIONS)  # NOTE: unused - voter wait times
+loc_ct = np.zeros(NUM_LOCATIONS)  # NOTE: unused
 
 # ===============================================================================
 # Utility Functions
 
 
-def voting_time_calcs(Ballot):
-    votemin = MinVotingMin + (MaxVotingMin - MinVotingMin) / \
-        (MaxBallot - MinBallot) * (Ballot - MinBallot)
-    votemode = MinVotingMode + \
-        (MaxVotingMode - MinVotingMode) / \
-        (MaxBallot - MinBallot) * (Ballot - MinBallot)
-    votemax = MinVotingMax + (MaxVotingMax - MinVotingMax) / \
-        (MaxBallot - MinBallot) * (Ballot - MinBallot)
-    voteavg = (votemin + votemode + votemax) / 3
+def voting_time_calcs(ballot):
+    vote_min = \
+        MIN_VOTING_MIN + \
+        (MAX_VOTING_MIN - MIN_VOTING_MIN) / \
+        (MAX_BALLOT - MIN_BALLOT) * \
+        (ballot - MIN_BALLOT)
 
-    return votemin, votemode, votemax, voteavg
+    vote_mode = \
+        MIN_VOTING_MODE + \
+        (MAX_VOTING_MODE - MIN_VOTING_MODE) / \
+        (MAX_BALLOT - MIN_BALLOT) * \
+        (ballot - MIN_BALLOT)
+
+    vote_max = \
+        MIN_VOTING_MAX + \
+        (MAX_VOTING_MAX - MIN_VOTING_MAX) / \
+        (MAX_BALLOT - MIN_BALLOT) * \
+        (ballot - MIN_BALLOT)
+
+    vote_avg = (vote_min + vote_mode + vote_max) / 3
+
+    return vote_min, vote_mode, vote_max, vote_avg
 
 
 def create_res_df(max_voters):
     '''
         This function is used to create an empty dataframe to store simulation results
     '''
-    res_cols = ["Replication", "Voter", "Arrival_Time", "Voting_Start_Time",
-                "Voting_Time", "Departure_Time", "Waiting_Time", "Total_Time", "Used"]
+    res_cols = [
+        'Replication',
+        'Voter',
+        'Arrival_Time',
+        'Voting_Start_Time',
+        'Voting_Time',
+        'Departure_Time',
+        'Waiting_Time',
+        'Total_Time',
+        'Used'
+    ]
     # Results dataframe with a record for the max possible number of voters
     voter_cols = np.zeros((max_voters, len(res_cols)))
     voter_results = pd.DataFrame(voter_cols, columns=res_cols)
     # Populates the voter ID field
-    voter_results['Voter'] = "Voter " + voter_results.index.astype('str')
+    voter_results['Voter'] = 'Voter ' + voter_results.index.astype('str')
 
     return voter_results
 
@@ -149,7 +166,7 @@ def create_loc_df(vote_locs):
     '''
         This function creates a dataframe of IZGBS results to be outputted
     '''
-    res_cols = ["Resource", "Exp. Avg. Wait Time", "Exp. Max. Wait Time"]
+    res_cols = ['Resource', 'Exp. Avg. Wait Time', 'Exp. Max. Wait Time']
     # Create an empty dataframe the same size as the locations dataframe
     voter_cols = np.zeros((vote_locs, len(res_cols)))
     loc_results = pd.DataFrame(voter_cols, columns=res_cols)
@@ -164,7 +181,7 @@ def create_hypothesis_df(num_h):
         This function creates a dataframe to store hypothesis testing results
     '''
     # This dataframe will contain 1 record per machine count
-    res_cols = ["Machines", "Feasible", "BatchAvg", "BatchAvgMax"]
+    res_cols = ['Machines', 'Feasible', 'BatchAvg', 'BatchAvgMax']
     # Create an empty dataframe the same size as the locations dataframe
     voter_cols = np.zeros((num_h, len(res_cols)))
     hyp_results = pd.DataFrame(voter_cols, columns=res_cols)
@@ -178,8 +195,14 @@ def create_hypothesis_df(num_h):
 # Main IZGBS Function
 
 
-# NOTE: this appears to be the main izgbs function - Collin
-def izgbs(vote_loc, total_num, start, min_val, alpha):
+# NOTE: this appears to be the main izgbs function
+def izgbs(
+    vote_loc,
+    total_num,
+    start,
+    min_val,
+    alpha
+):
     '''
         Main IZGBS function. TODO: fill out function desc
 
@@ -216,15 +239,28 @@ def izgbs(vote_loc, total_num, start, min_val, alpha):
     cur_upper = total_num
 
     while (hyps_rem):
-        print("Num Test:" + str(num_test))
-        print("Current Upper:" + str(cur_upper))
-        print("Current Lower" + str(cur_lower))
+        print(f'Num Test: {num_test}')
+        print(f'Current Upper: {cur_upper}')
+        print(f'Current Lower {cur_lower}')
+
         first_rep = 1
-        for i in range(1, num_replications):
+
+        for i in range(1, NUM_REPLICATIONS):
             results_df = create_res_df(voters_max)
+
             # calculate voting times
-            results_df = voter_sim(results_df, voters_max, vote_min, vote_mode,
-                                   vote_max, poll_start, poll_end, arrival_rt, num_test, loud=0)
+            results_df = voter_sim(
+                results_df,
+                voters_max,
+                vote_min,
+                vote_mode,
+                vote_max,
+                POLL_START,
+                POLL_END,
+                arrival_rt,
+                num_test,
+                loud=0
+            )
 
             # only keep results records that were actually used
             results_df = results_df[results_df['Used'] == 1].copy()
@@ -238,17 +274,16 @@ def izgbs(vote_loc, total_num, start, min_val, alpha):
 
         # calculate waiting time
         # need to drop voters that did not finish
-        final_loc_results['Waiting_Time'] = final_loc_results['Voting_Start_Time'] - \
-            final_loc_results['Arrival_Time']
-        final_loc_results['Total_Time'] = final_loc_results['Departure_Time'] - \
-            final_loc_results['Arrival_Time']
+        final_loc_results['Waiting_Time'] = \
+            final_loc_results['Voting_Start_Time'] - final_loc_results['Arrival_Time']
+
+        final_loc_results['Total_Time'] = \
+            final_loc_results['Departure_Time'] - final_loc_results['Arrival_Time']
 
         # calculate average and max waiting time by replication
         rep_nums = final_loc_results.groupby(
             ['Replication']
-        )['Waiting_Time'].agg(
-            ['max', 'mean']
-        ).reset_index()
+        )['Waiting_Time'].agg(['max', 'mean']).reset_index()
 
         # rename fields
         rep_nums.rename(
@@ -257,7 +292,7 @@ def izgbs(vote_loc, total_num, start, min_val, alpha):
         )
 
         # assign records to batches
-        rep_nums['Batch'] = rep_nums['Replication'] % num_batches
+        rep_nums['Batch'] = rep_nums['Replication'] % NUM_BATCHES
         rep_nums.head()
 
         # calculate batch averages
@@ -279,8 +314,8 @@ def izgbs(vote_loc, total_num, start, min_val, alpha):
 
         # calculate test statistic
         if (max_std > 0):
-            z = (max_avg - (service_req + deltaIZ)) / \
-                (max_std / math.sqrt(num_batches))
+            z = (max_avg - (SERVICE_REQ + DELTA_IZ)) / \
+                (max_std / math.sqrt(NUM_BATCHES))
             p = st.norm.cdf(z)
 
             # feasible
@@ -368,28 +403,40 @@ if __name__ == '__main__':
     voting_machines['ID'] = voting_machines['ID'].astype('int')
 
     voting_machines['Arrival_mean'] = \
-        voting_machines['Likely or Exp. Voters'] / poll_open / 60
+        voting_machines['Likely or Exp. Voters'] / POLL_OPEN / 60
 
     voting_machines.head()
 
     # Create a dataframe to store location results
 
     num_machines = 50
-    loc_df_results = create_loc_df(num_locations + 1)
+    loc_df_results = create_loc_df(NUM_LOCATIONS + 1)
 
-    for i in range(1, num_locations):
+    for i in range(1, NUM_LOCATIONS):
         logger.info(f'Starting Location: {i}')
 
         # Placeholder, use a different start value for later machines?
-        if (min_alloc_flg):
-            start_val = math.ceil((max_machines - min_alloc) / 2) + min_alloc
-            SASalphaValue = alphaValue / math.log2(max_machines - min_alloc)
-            loc_res = izgbs(i, max_machines, start_val,
-                            min_alloc, SASalphaValue)
+        if (MIN_ALLOC_FLG):
+            start_val = math.ceil((MAX_MACHINES - MIN_ALLOC) / 2) + MIN_ALLOC
+            SASalphaValue = ALPHA_VALUE / math.log2(MAX_MACHINES - MIN_ALLOC)
+
+            loc_res = izgbs(
+                i,
+                MAX_MACHINES,
+                start_val,
+                MIN_ALLOC,
+                SASalphaValue
+            )
         else:
-            start_val = math.ceil((max_machines - 1) / 2)
-            SASalphaValue = alphaValue / math.log2(max_machines - min_alloc)
-            loc_res = izgbs(i, num_machines, start_val, 2, SASalphaValue)
+            start_val = math.ceil((MAX_MACHINES - 1) / 2)
+            SASalphaValue = ALPHA_VALUE / math.log2(MAX_MACHINES - MIN_ALLOC)
+            loc_res = izgbs(
+                i,
+                num_machines,
+                start_val,
+                2,
+                SASalphaValue
+            )
 
         loc_feas = loc_res[loc_res['Feasible'] == 1].copy()
 
