04b46f03625b11b9e25c6e68b5c2a70121903088
Ghostjs123 <31968293+Ghostjs123@users.noreply.github.com>
2021-10-07 21:59:22 -0400

    Refactored Settings into a class shared btwn all files (#6)

--- src/fetch_location_data.py
+++ src/fetch_location_data.py
@@ -1,5 +1,6 @@
-import settings
 from xlrd import Book
+
+from src.settings import Settings
 
 
 COLUMN_NAMES = ['Likely or Exp. Voters', 'Eligible Voters', 'Ballot Length Measure']
@@ -7,13 +8,13 @@ COLUMN_NAMES = ['Likely or Exp. Voters', 'Eligible Voters', 'Ballot Length Measu
 
 def fetch_location_data(voting_config: Book) -> dict:
     '''
-        Fetches the locations sheet from the input xlsx as a DataSet.
+        Fetches the locations sheet from the input xlsx as a dict.
 
         Params:
             voting_config (xlrd.Book) : input xlsx sheet.
 
         Returns:
-            (DataSet) : location sheet as a DataSet.
+            (dict) : location sheet as a dict.
     '''
     location_sheet = voting_config.sheet_by_name(u'locations')
 
@@ -26,92 +27,7 @@ def fetch_location_data(voting_config: Book) -> dict:
         data = map(int, location_sheet.row_values(i)[1:])  # [1:] drops ID column
         location_data[i] = dict(zip(COLUMN_NAMES, data))
         location_data[i].update({
-            'Arrival Mean': location_data[i]['Likely or Exp. Voters'] / settings.POLL_OPEN / 60
+            'Arrival Mean': location_data[i]['Likely or Exp. Voters'] / Settings.POLL_OPEN / 60
         })
 
     return location_data
-
-
-# def clean_location_data(location_data: dict) -> None:
-#     '''
-#         Updates the location_data read in from the input xlsx.
-
-#         Params:
-#             location_data (DataSet) : locations tab from input xlsx.
-
-#         Returns:
-#             None.
-#     '''
-#     # sort voting location for optimization
-#     location_data.sort_values(
-#         ['Likely or Exp. Voters', 'Eligible Voters', 'Ballot Length Measure'],
-#         ascending=False
-#     )
-
-#     # create location ID specific to new sort order
-#     location_data['Loc_ID'] = [int(row[0] + 1) for row in location_data]
-#     # location_data['Loc_ID'] = (location_data.index + 1).astype('int')
-
-#     # convert columns to numeric so they can be used for calculations
-#     location_data['Likely or Exp. Voters'] = list(map(int, location_data['Likely or Exp. Voters']))
-#     location_data['Eligible Voters'] = list(map(int, location_data['Eligible Voters']))
-#     location_data['Ballot Length Measure'] = list(map(int, location_data['Ballot Length Measure']))
-
-#     # convert ID to int
-#     location_data['ID'] = list(map(int, location_data['ID']))
-
-#     location_data['Arrival_mean'] = [
-#         v / settings.POLL_OPEN / 60
-#         for v in location_data['Likely or Exp. Voters']
-#     ]
-
-
-# def old_fetch_location_data(voting_config: Book) -> list:
-#     location_sheet = voting_config.sheet_by_name(u'locations')
-
-#     location_data = []
-
-#     for i in range(location_sheet.nrows):
-#         location_data.append(
-#             location_sheet.row_values(
-#                 i,
-#                 start_colx=0,
-#                 end_colx=None
-#             )
-#         )
-
-#     return location_data
-
-
-# def old_clean_location_data(location_data: list) -> None:
-#     import pandas as pd
-
-#     voting_machines = pd.DataFrame(location_data)
-#     voting_machines.columns = voting_machines.iloc[0]
-#     voting_machines = voting_machines.iloc[1:]
-
-#     # sort voting location for optimization
-#     voting_machines.sort_values(
-#         ['Likely or Exp. Voters', 'Eligible Voters', 'Ballot Length Measure'],
-#         ascending=False,
-#         inplace=True
-#     )
-
-#     # create location ID specific to new sort order
-#     voting_machines['Loc_ID'] = (voting_machines.index + 1).astype('int')
-
-#     # convert columns to numeric so they can be used for calculations
-#     voting_machine_nums = [
-#         'Likely or Exp. Voters',
-#         'Eligible Voters',
-#         'Ballot Length Measure'
-#     ]
-#     voting_machines[voting_machine_nums] = voting_machines[voting_machine_nums].astype('int')
-
-#     # convert ID to int
-#     voting_machines['ID'] = voting_machines['ID'].astype('int')
-
-#     voting_machines['Arrival_mean'] = \
-#         voting_machines['Likely or Exp. Voters'] / 13 / 60
-
-#     return voting_machines
